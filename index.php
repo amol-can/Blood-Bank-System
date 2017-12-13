f0c9d49a38d6a54b552ef0b872b8f53b657e10c2
Amol Patil <computer.warriors2020@gmail.com>
2017-12-13 16:54:10 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Blood Bank System</title>
+	<meta charset=utf-8>
+	<meta name=description content="Designed a simple ‘Blood Bank System’ web application">
+	<meta name=viewport content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+	<!-- This block is for Navigation Bar of Index page -->
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+		    <div class="navbar-header">
+		      <a class="navbar-brand" href="#">Blood Bank</a>
+		    </div>
+		    <ul class="nav navbar-nav">
+		      <li class="active"><a href="#">Home</a></li>
+		      <li><a href="#">Page 1</a></li>
+		      <li><a href="#">Page 2</a></li>
+		      <li><a href="#">Page 3</a></li>
+		    </ul>
+		    <ul class="nav navbar-nav navbar-right">
+		    	<li>
+		    		<a class="dropdown-toggle" data-toggle="dropdown" href="#">Login
+				    <span class="caret"></span></a>
+				    <ul class="dropdown-menu">
+				       	<li><a href="#">Hospital</a></li>
+				        <li><a href="#">Receiver</a></li>
+				    </ul>
+		    	</li>
+
+		    	<li class="dropdown">
+			      	<a class="dropdown-toggle" data-toggle="dropdown" href="#">Register
+				    <span class="caret"></span></a>
+				    <ul class="dropdown-menu">
+				       	<li><a href="#">New Hospital</a></li>
+				        <li><a href="#">New Receiver</a></li>
+				    </ul>
+			   </li>
+		    </ul>
+	  </div>
+	</nav>
+	<!-- //End of Navigation bar block -->
+
+</body>
+</html>
